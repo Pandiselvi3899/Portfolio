40b02808e7dde64683a7cc1938b4d896f6c49449
Pandi Selvi <89200308+Pandiselvi3899@users.noreply.github.com>
2022-03-10 14:08:45 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,16 +23,18 @@ function Projects() {
         <strong className="purple">Credentials</strong>
         <p style={{ color: "white" }}>Username : selvi , Email : selvi@gmail.com , Password : 12345</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+
+        <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={pizza}
-              title="PIZZA CORNER🍕"
-              description="It is a Pizza delivery web app which allows you to select your favourite pizza.Add to Cart your pizzas and place your orders in online"
-              link="https://piz-corner.herokuapp.com/"
-              link1="https://github.com/Pandiselvi3899/PizzaCorner-FE"
-              link2="https://github.com/Pandiselvi3899/PizzaCorner-BE"
+              imgPath={bus}
+              title="BUS TICKET BOOKING APP🚌"
+              description="This App is used for booking your tickets to travel your favourite places. List of cities for users to choose from starting city and destination city."
+              link="https://trusting-bardeen-717927.netlify.app"
+              link1="https://github.com/Pandiselvi3899/BT-frontend"
+              link2="https://github.com/Pandiselvi3899/BT-backend"
             />
           </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={shop}
@@ -46,23 +48,12 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bus}
-              title="BUS TICKET BOOKING APP🚌"
-              description="This App is used for booking your tickets to travel your favourite places. List of cities for users to choose from starting city and destination city."
-              link="https://trusting-bardeen-717927.netlify.app"
-              link1="https://github.com/Pandiselvi3899/BT-frontend"
-              link2="https://github.com/Pandiselvi3899/BT-backend"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chat}
-              title="WETALK💬"
-              description="It is a Full Stack Real time Chatting App.We have one on one chat as well as group chatting functionality in this app.Chat with your loved one."
-              link="https://ps-we-chat.herokuapp.com/"
-              link1="https://github.com/Pandiselvi3899/chat-frontend"
-              link2="https://github.com/Pandiselvi3899/chat-backend"
+              imgPath={pizza}
+              title="PIZZA CORNER🍕"
+              description="It is a Pizza delivery web app which allows you to select your favourite pizza.Add to Cart your pizzas and place your orders in online"
+              link="https://piz-corner.herokuapp.com/"
+              link1="https://github.com/Pandiselvi3899/PizzaCorner-FE"
+              link2="https://github.com/Pandiselvi3899/PizzaCorner-BE"
             />
           </Col>
 
@@ -74,6 +65,16 @@ function Projects() {
               link="https://bk-ps.herokuapp.com/"
               link1="https://github.com/Pandiselvi3899/Bookkeeping-app-FE"
               link2="https://github.com/Pandiselvi3899/Bookkeeping-app-BE"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chat}
+              title="WETALK💬"
+              description="It is a Full Stack Real time Chatting App.We have one on one chat as well as group chatting functionality in this app.Chat with your loved one."
+              link="https://ps-we-chat.herokuapp.com/"
+              link1="https://github.com/Pandiselvi3899/chat-frontend"
+              link2="https://github.com/Pandiselvi3899/chat-backend"
             />
           </Col>
         </Row>
